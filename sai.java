79eb6beb4d4ed4e52105947cc06365087bfc4691
sai <vinay.sunkara95@gmail.com>
2023-06-15 14:26:45 +0000

    f2

--- sai.java
+++ sai.java
@@ -3,6 +3,7 @@ public class Sai
 	public static void main(String args[])
 	{
 		System.out.println("hello");
-		System.out.println("hi);
+		System.out.println("hi");
+		System.out.println("congrats");
 	}
 }
